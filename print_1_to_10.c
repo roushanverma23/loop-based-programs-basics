190934b64be2d233521d8078f525583728b25b00
ROUSHAN VERMA <roushanvermanitt23@gmail.com>
2023-07-23 23:38:17 +0530

    day 1

---
+++ print_1_to_10.c
@@ -0,0 +1,11 @@
+// program to print 1 to 10 using loop
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    system("cls");
+    for(int i=1; i<=10; i++){
+        printf("%d\t",i);
+    }
+    return 0;
+}
